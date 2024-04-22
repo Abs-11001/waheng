d8ebec97c838ff5d40fd68c27011cb46f731f6e7
Abs-11001 <1196399230@qq.com>
2024-04-22 15:59:32 +0800

    完善card组件表现样式

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -5,11 +5,12 @@
               width="40"
               height="40"
               fit="contain"
+              show-loader
               :src="avatar"
             />
         </div>
         <div class="info">
-            <div class="title">GitHub</div>
+            <div class="title" data-hover="GitHubGitHubGitHubGitHub">GitHubGitHubGitHubGitHub</div>
             <div class="des">全球最大同性交友网站</div>
         </div>
         <div class="arrow">
@@ -34,9 +35,13 @@ const avatar = new URL('../assets/github.png', import.meta.url).href
     box-shadow: 0 0 20px -5px rgba(158,158,158,.2);
     transition: linear .1s;
     .icon {
-
+      .arco-image{
+        opacity: .85;
+        transition: opacity .35s,transform .35s;
+      }
     }
     .info {
+      position: relative;
       flex: 1;
       margin-left: 10px;
       overflow: hidden;
@@ -47,6 +52,20 @@ const avatar = new URL('../assets/github.png', import.meta.url).href
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
+        transition: .3s all
+      }
+      .title:before {
+        background-image: linear-gradient(to right,#026abd,#4caf50);
+        -webkit-background-clip: text;
+        color: transparent;
+        content: attr(data-hover);
+        position: absolute;
+        opacity: 0;
+        transform: scale(1.1) translateX(10px) translateY(-10px) rotate(4deg);
+        -webkit-transition: -webkit-transform .3s,opacity .3s;
+        -moz-transition: -moz-transform .3s,opacity .3s;
+        transition:transform .3s,opacity .3s;
+        pointer-events: none
       }
       .des{
         font-size: .75rem;
@@ -67,8 +86,16 @@ const avatar = new URL('../assets/github.png', import.meta.url).href
     box-shadow:0 26px 40px -24px rgba(0,36,100,.3);
     transform: translateY(-4px);
   }
+  .card:hover .arco-image{
+    opacity: 1;
+    transform: scale3d(1.2,1.2,1);
+  }
   .card:hover .title{
-    color: red;
+    color: transparent;
+  }
+  .card:hover .title:before{
+    transform: scale(1) translateX(0) translateY(0) rotate(0);
+    opacity: 1
   }
   .card:hover .arrow{
     opacity: 1;
